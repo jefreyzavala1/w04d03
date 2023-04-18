a21c7938ff18c14d6488018c3574e5bc1da9d1c9
jefreyzavala1 <jefreyzavala1@gmail.com>
2023-04-18 17:34:15 -0400

    Create Hamster Class

--- homework.js
+++ homework.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+class Hamster{
+    constructor(name){
+        this.owner = '',
+        this.name = name,
+        this.price = 15
+    }
+    wheelRun(){
+        console.log("squeak squeak")
+    }
+    eatFood(){
+        console.log("nibble nibble")
+    }
+    getPrice(){
+        return this.price
+    }
+}
+
