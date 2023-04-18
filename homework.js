b5d3330ae0932b681e7267b58850321ce52d9d79
jefreyzavala1 <jefreyzavala1@gmail.com>
2023-04-18 17:43:34 -0400

    Create the Person class

--- homework.js
+++ homework.js
@@ -15,3 +15,46 @@ class Hamster{
     }
 }
 
+class Person{
+    constructor(name){
+        this.name = name,
+        this.age = 0,
+        this.height = 0,
+        this.weight = 0,
+        this.mood = 0,
+        this.hamsters = [],
+        this.bankAccount = 0
+    }
+    getName(){
+        return this.name
+    }
+    getAge(){
+        return this.age
+    }
+    getWeight(){
+        return this.weight
+    }
+    greet(){
+        console.log(`Hi there my name is ${this.name}`)
+    }
+    eat(){
+        this.weight+=5,
+        this.mood+=2
+    }
+    exercise(){
+        this.weight-=4
+    }
+    ageUp(){
+        this.age+=1,
+        this.height+=2,
+        this.weight+=3,
+        this.mood-=1,
+        this.bankAccount+=10
+    }
+    buyHamster(hamster){
+        this.hamsters.push(hamster);
+        this.mood+=10,
+        this.bankAccount-=hamster.getPrice()
+    }
+}
+
