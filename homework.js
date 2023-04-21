aba730f7506a15649299577991a6c5e3424b3f7e
jefreyzavala1 <jefreyzavala1@gmail.com>
2023-04-21 00:56:36 -0400

    Complete task 4: create chef factory and Dinner class

--- homework.js
+++ homework.js
@@ -1,3 +1,4 @@
+//Task 1: Create Hamster Class
 class Hamster {
   constructor(name) {
     (this.owner = ""), (this.name = name), (this.price = 15);
@@ -20,6 +21,7 @@ hammy.eatFood();
 hammy.getPrice();
 console.log(hammy.getPrice());
 
+//Task 2: Create Person Class
 class Person {
   constructor(name) {
     (this.name = name),
@@ -83,6 +85,7 @@ person1.age = 20;
 person1.height = 55;
 (person1.weight = 100), (person1.mood = 2), console.log(person1);
 
+//Task 3: Create a Story with Person Class
 const timmy = new Person("Timmy");
 console.log(timmy)
 ageMe(timmy, 5);
@@ -109,4 +112,33 @@ console.log(timmy)
 eatCount(timmy, 2)
 exerciseTimes(timmy,2)
 console.log(timmy)
+
+//Task 4: Create Chef Factory and Dinner Class
+class Dinner{
+   constructor(appetizer,entree,dessert){
+    this.appetizer =appetizer,
+    this.entree = entree
+    this.dessert =  dessert
+   }
+}
+
+class Chef{
+   makeDinner(appetizer,entree,dessert){
+    return new Dinner(appetizer,entree,dessert)
+   }
+}
+
+const chef1 = new Chef();
+const chef2 = new Chef();
+const chef3 = new Chef();
+
+const dinner1 = chef1.makeDinner('wings','pasta','chocolate fudge')
+const dinner2 = chef1.makeDinner('fried calamari','smoked salmon with a side of rice','cheesecake')
+const dinner3 = chef3.makeDinner('nachos','steak with a side of broccoli and smashed potatoes','ice cream cake')
+
+console.log(dinner1)
+console.log(dinner2)
+console.log(dinner3)
+
+
 
